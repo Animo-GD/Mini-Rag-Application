aebca73b18bca6c0c876b1c24a4028321007a9a8
Moaaz <mo3azsolomon@gmail.com>
2025-11-10 17:49:51 +0200

    MongoDB Schemes

---
+++ src/models/ChunkModel.py
@@ -0,0 +1,39 @@
+
+from models.BaseDataModel import BaseDataModel
+from models.db_schemes import DataChuck
+from models.enums import DataBaseEnum
+from bson.objectid import ObjectId
+from pymongo import InsertOne
+
+
+class ChunkModel(BaseDataModel):
+    def __init__(self, db_client):
+        super().__init__(db_client)
+        self.collection = self.db_client[DataBaseEnum.COLLECTION_CHUNK_NAME.value]
+
+    async def create_chunk(self,chunk:DataChuck):
+        result = await self.collection.insert_one(chunk.model_dump())
+        chunk._id = result.inserted_id
+        return chunk
+    
+    async def get_chunk(self,chunk_id:str):
+        result = await self.collection.find_one({
+            "_id":ObjectId(chunk_id)
+        })
+
+        if result is None:
+            return None
+        
+        return DataChuck(**result)
+    async def insert_many_chunks(self,chunks:list,batch_size:int=100):
+        for i in range(0,len(chunks),batch_size):
+            batch = chunks[i:i+batch_size]
+
+            operations = [
+                InsertOne(chunk.model_dump())
+                for chunk in batch
+            ]
+
+            await self.collection.bulk_write(operations)
+
+        return len(chunks)
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter,Depends,UploadFile,status
+from fastapi import APIRouter,Depends,UploadFile,status,Request
 from fastapi.responses import JSONResponse
 from utilis.config import get_settings,Setting
 from controllers import DataController,ProjectController,ProcessController
 from models import ResponseSignal
 from routes.schemes.data import ProccessRequest
+from models.ProjectModel import ProjectModel
+from models.db_schemes import DataChuck
+from models.ChunkModel import ChunkModel
 import aiofiles
 import logging
 
@@ -15,7 +18,11 @@ data_router = APIRouter(
 )
 
 @data_router.post("/upload/{project_id}")
-async def upload_data(project_id:str,file:UploadFile,app_settings:Setting = Depends(get_settings)):
+async def upload_data(request:Request,project_id:str,file:UploadFile,app_settings:Setting = Depends(get_settings)):
+
+    project_model = ProjectModel(db_client=request.app.db_client)
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+
     data_ctrl=DataController()
     # Validate Files
     is_valid,return_msg = data_ctrl.validate_file(file=file)
@@ -53,11 +60,17 @@ async def upload_data(project_id:str,file:UploadFile,app_settings:Setting = Depe
               )
 
 @data_router.post("/process/{project_id}")
-async def process_endpoint(project_id:str,process_request:ProccessRequest):
+async def process_endpoint(request:Request,project_id:str,process_request:ProccessRequest):
     file_id = process_request.file_id
     chunk_size = process_request.chunk_size
     overlap_size = process_request.overlap_size
     process_controller = ProcessController(project_id=project_id)
+
+    project_model = ProjectModel(db_client=request.app.db_client)
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+
+
+
     file_content = process_controller.get_file_content(file_id=file_id)
 
     file_chunks = process_controller.process_file_content(file_content=file_content,file_id=file_id
@@ -71,4 +84,16 @@ async def process_endpoint(project_id:str,process_request:ProccessRequest):
             }
         )
     
-    return file_chunks
+    file_chunks_records = [
+        DataChuck(
+            chunk_text=chunk.page_content,
+            chunk_metadata=chunk.metadata,
+            chunk_order=i+1,
+            chunk_project_id=project.id
+        )
+        for i,chunk in enumerate(file_chunks)
+    ]
+    chunk_model = ChunkModel(db_client=request.app.db_client)
+
+    no_records = await chunk_model.insert_many_chunks(chunks=file_chunks_records)
+    return no_records
